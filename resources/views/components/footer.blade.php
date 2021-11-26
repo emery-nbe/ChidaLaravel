4073aeb10c37035ed48d579968b357cd8b4dbd02
Emery <emeryntumba39@outlook.com>
2021-11-26 14:33:33 +0100

    Initial commit

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,47 @@
+
+<footer>
+    <div class="footer-container">
+
+        <div class="newsletter">
+            <input type="email" placeholder="your email address">
+            <button type="submit">S'enregistrer</button>
+        </div>
+
+        <div class="footer-services">
+            <div class="footer-custom">
+                <h3>Customer Services</h3>
+                <p>Buy</p>
+                <p>Reduction</p>
+                <p>Bonus</p>
+            </div>
+            <div class="footer-about">
+                <h3>About Us</h3>
+                <p>Buy</p>
+                <p>Reduction</p>
+                <p>Bonus</p>
+            </div>
+            <div class="footer-sell">
+                <h3>Sell on Chida</h3>
+                <p>Buy</p>
+                <p>Reduction</p>
+                <p>Bonus</p>
+            </div>
+        </div>
+
+        <div class="footer-text">
+            <span>Get on
+                <img src="img/playstore_60px.png" width="30px" height="30px">
+                <img src="img/app_store_64px.png" width="30px" height="35px">
+            </span>
+            <span>Follow us on
+                 <img src="img/facebook_60px.png" width="30px" height="30px">
+                 <img src="img/twitter_60px.png" width="30px" height="30px">
+                <img src="img/instagram_60px.png" width="30px" height="30px">
+            </span>
+        </div>
+        <div class="footer-copyright">
+            <span>©2021 Tout droit reservé</span>
+        </div>
+    </div>
+
+</footer>
---
+++ resources/views/components/header.blade.php
@@ -0,0 +1,29 @@
+<header>
+    <nav>
+        <div class="nav-container">
+            <div class="logo">
+                <a href="index.php"><p>Chida</p></a>
+            </div>
+            <div class="search-bar">
+                <select>
+                    <option>Categorie</option>
+                    <option>Bureautique</option>
+                    <option>Cuisine</option>
+                    <option>Habillement</option>
+                    <option>Fast Food</option>
+                    <option>Gaming</option>
+                    <option>Boisson</option>
+                </select>
+
+                <input name="search" placeholder="Que recherchez-vous ?">
+
+            </div>
+            <div class="nav-menu">
+                <ul>
+                    <li><a href="index.php"><button class="btn-nav btn-home"></button></a></li>
+                    <li><a href="connexion.php"><button class="btn-nav btn-person"></button></a></li>
+                    <li><a href="command.php"><button class="btn-nav btn-market"><span id="counter"></span></button></a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
